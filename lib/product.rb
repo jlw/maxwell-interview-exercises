9e9f377df4d11479446118d5d7ec31b0d7ff8761
Jeremy Weathers <jeremy@codekindly.com>
2020-07-06 11:34:58 -0500

    Add initial Product with cost breakdown
    - handle both sale and non-sale products
    - copy product name for ease in reporting

---
+++ lib/product.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+ProductCost = Struct.new :name, :quantity, :total, :savings
+
+class Product
+  attr_reader :name, :unit_price, :sale_quantity, :sale_price
+
+  def initialize(name:, unit_price:, sale_quantity: nil, sale_price: nil)
+    @name = name
+    @unit_price = unit_price
+    @sale_quantity = sale_quantity
+    @sale_price = sale_price
+  end
+
+  def cost_breakdown_for(quantity)
+    product_cost = ProductCost.new name, quantity, unit_price * quantity, 0
+    return product_cost if sale_quantity.nil? || sale_price.nil?
+
+    calculate_sale_cost(product_cost)
+  end
+
+  private
+
+  def calculate_sale_cost(product_cost)
+    base_cost = product_cost.total
+    bundles, full_cost_units = product_cost.quantity.divmod(sale_quantity)
+
+    product_cost.total = (bundles * sale_price) + (full_cost_units * unit_price)
+    product_cost.savings = base_cost - product_cost.total
+    product_cost
+  end
+end
---
+++ spec/lib/product_spec.rb
@@ -0,0 +1,23 @@
+require 'product'
+
+RSpec.describe Product do
+  it 'calculates non-sale prices with product info' do
+    product = Product.new name: 'Artichoke', unit_price: 7
+    cost = product.cost_breakdown_for(3)
+
+    expect(cost.name).to eq 'Artichoke'
+    expect(cost.quantity).to eq 3
+    expect(cost.total).to eq 21
+    expect(cost.savings).to eq 0
+  end
+
+  it 'calculates sale prices with product info' do
+    product = Product.new name: 'Beet', unit_price: 5, sale_quantity: 3, sale_price: 12
+    cost = product.cost_breakdown_for(4)
+
+    expect(cost.name).to eq 'Beet'
+    expect(cost.quantity).to eq 4
+    expect(cost.total).to eq 17
+    expect(cost.savings).to eq 3
+  end
+end
